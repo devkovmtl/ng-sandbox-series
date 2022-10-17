b25954582d04cd8f5e0d415ee886b7a4c83d6dae
Mikael <dev.kov.mtl@gmail.com>
2022-10-17 11:16:17 -0400

    Angular - Communication between component  child to parent

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -8,7 +8,15 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ChildComponent implements OnInit {
   @Input() childMessage: string = '';
 
+  @Output() messageEvent = new EventEmitter<string>();
+
+  msg = 'message from child';
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  sendMessage() {
+    this.messageEvent.emit('Hello from child');
+  }
 }
--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { ChildComponent } from '../child/child.component';
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
-  styleUrls: ['./parent.component.scss']
+  styleUrls: ['./parent.component.scss'],
 })
-export class ParentComponent implements OnInit {
+export class ParentComponent implements OnInit, AfterViewInit {
+  @ViewChild(ChildComponent) child: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    console.log(this.child.msg);
   }
 
+  receiveMessage(message: string): void {
+    alert(message);
+  }
 }
