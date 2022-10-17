88eca0703e1e27793698f0e61b81a209b95b48f3
Mikael <dev.kov.mtl@gmail.com>
2022-10-17 10:34:33 -0400

    Angular - Lifecycle hooks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  intervalSub: any;
+
+  ngOnInit(): void {
+    this.intervalSub = setInterval(() => {
+      console.log('Hello form ngOnInit');
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalSub) {
+      clearInterval(this.intervalSub);
+    }
+  }
+
   title = 'angular-series';
 }
